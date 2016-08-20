e033e5ee1395a1e3f4eb066e51de80109c35084a
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2016-08-20 18:01:25 +0100

    templates working but unable to post

--- app/js/app.js
+++ app/js/app.js
@@ -7,6 +7,7 @@ EmpireApp.API_URL = "http://localhost:3000/api";
 EmpireApp.setRequestHeader = function(jqXHR) {
   var token = window.localStorage.getItem("token");
   if(!!token) return jqXHR.setRequestHeader('Authorization', 'Bearer ' + token);
+ 
 }
 
 EmpireApp.getTemplate = function(template, data) {
@@ -16,6 +17,43 @@ EmpireApp.getTemplate = function(template, data) {
     EmpireApp.updateUI();
   });
 }
+
+EmpireApp.handleForm = function(){
+  event.preventDefault();
+  console.log("going to url");
+  $(this).find('button').prop('disabled', true);
+
+  var data = $(this).serialize();
+  var method = $(this).attr("method");
+
+  var url = EmpireApp.API_URL + $(this).attr("action");
+
+
+  return $.ajax({
+    url: url,
+    method: method,
+    data: data,
+    beforeSend: EmpireApp.setRequestHeader
+  })
+  .done(function(data){
+    if(!!data.token){
+      window.localStorage.setItem("token", data.token);
+    }
+
+  })
+
+  .fail(EmpireApp.handleFormErrors);
+}
+
+EmpireApp.handleFormErrors = function(jqXHR){
+  console.log("going to url");
+  var $form = $("form");
+  for(field in jqXHR.responseJSON.errors){
+     $form.find('input[name=' + field + ']').parents('.form-group').addClass('has-error');
+   }
+   $form.find('button').removeAttr('disabled');
+
+ }
 
 EmpireApp.updateUI = function() {
   var loggedIn = !!window.localStorage.getItem("token");
@@ -28,3 +66,30 @@ EmpireApp.updateUI = function() {
    $('.logged-out').removeClass("hidden"); 
   }
 }
+
+
+
+
+
+EmpireApp.loadPage = function(){
+  event.preventDefault();
+  console.log("page load")
+  EmpireApp.getTemplate($(this).data('template'));
+}
+
+EmpireApp.initEventHandlers = function() {
+  this.$main = $("main");
+
+  $(".navbar-nav a").not(".logout").on("click", this.loadPage);
+  $(".navbar-nav a.logout").on("click", this.logout);
+
+}
+
+EmpireApp.init = function(){
+  this.initEventHandlers();
+  this.updateUI();
+  console.log("we're off");
+  }.bind(EmpireApp);
+
+
+$(EmpireApp.init);
