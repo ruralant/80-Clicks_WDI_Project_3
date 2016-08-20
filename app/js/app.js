5d49bb071363cdf8012e0846ce49c2fc1f44e414
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2016-08-20 19:32:12 +0100

    user log in fixed need to do full test

--- app/js/app.js
+++ app/js/app.js
@@ -79,9 +79,13 @@ EmpireApp.loadPage = function(){
 
 EmpireApp.initEventHandlers = function() {
   this.$main = $("main");
+  this.$main.on("submit", "form", this.handleForm);
 
   $(".navbar-nav a").not(".logout").on("click", this.loadPage);
   $(".navbar-nav a.logout").on("click", this.logout);
+  this.$main.on("focus", "form input", function(){
+    $(this).parents('.form-group').removeClass('has-error');
+  })
 
 }
 
