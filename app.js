38f5007538715d84900dd4fc7466bf01412a87e4
chetan barot <chetanbarot@hotmail.com>
2016-08-25 20:59:17 +0100

    Fogot the static folder!

--- app.js
+++ app.js
@@ -21,18 +21,9 @@ app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static("public"));
 
 app.use('/api', routes);
-
-app.get('/images/tank-icon/:color/:tanks', function(req, res) {
-  ejs.renderFile('./views/tank-icon.ejs', { color: "#" + req.params.color, tanks: req.params.tanks }, function(err, svg) {
-    if(err) res.status(500).json(err);
-
-    res.set('Content-Type', 'image/svg+xml')
-      .status(200)
-      .send(svg);
-  });
-});
 
 app.listen(port, function(){
   console.log("Yeah! Express is listening on port " + port);
