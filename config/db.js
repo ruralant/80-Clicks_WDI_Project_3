609356e72517d2f10bd679d766c681449e9d37a4
chetan barot <chetanbarot@hotmail.com>
2016-08-25 20:57:23 +0100

    Can't spell production

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 dbURIs = {
   test: "mongodb://localhost/userauth-test",
   development: 'mongodb://localhost/userauth',
-  prodcution: process.env.MONGODB_URI || "mongodb://localhost/userauth"
+  production: process.env.MONGODB_URI || "mongodb://localhost/userauth"
 }
 
 module.exports = function (env) {
