16b1f0be100a4a2f2cedf0e800eb4d9ff0f9d37d
chetan barot <chetanbarot@hotmail.com>
2016-08-25 20:50:44 +0100

    Updated mongo uri

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 dbURIs = {
   test: "mongodb://localhost/userauth-test",
   development: 'mongodb://localhost/userauth',
-  prodcution: process.env.MONGOLAB_URI || "mongodb://localhost/userauth"
+  prodcution: process.env.MONGODB_URI || "mongodb://localhost/userauth"
 }
 
 module.exports = function (env) {
